7af113847f8dfc48127c43a9dbadd18f5e818dce
Melih Odabas <melih_odabas@hotmail.com>
2025-01-16 18:45:07 +0300

    - start adding summary information for package documentation - add build properties to generate package documentation - add license agreement requirement while adding/updating packages - add existing license file to solution items

--- src/ModResults/ErrorExtensions.cs
+++ src/ModResults/ErrorExtensions.cs
@@ -1,6 +1,13 @@
 ﻿namespace ModResults;
 public static class ErrorExtensions
 {
+  /// <summary>
+  /// Checks whether the error has been constructed from an exception of the specified type.
+  /// </summary>
+  /// <typeparam name="TException">Exception type</typeparam>
+  /// <param name="error"></param>
+  /// <param name="includeAssignableFrom">If true, checks whether ,nput exception type is assignable from exception contained by error instance. If false, only checks for exact match.</param>
+  /// <returns></returns>
   public static bool HasException<TException>(this Error error, bool includeAssignableFrom = false)
     where TException : Exception
   {
@@ -30,6 +37,13 @@ public static class ErrorExtensions
     return false;
   }
 
+  /// <summary>
+  /// Checks whether the error has specified error code.
+  /// </summary>
+  /// <param name="error"></param>
+  /// <param name="code">Error code to check.</param>
+  /// <param name="comparisonType">One of the enumeration values that specifies how the strings will be compared.</param>
+  /// <returns></returns>
   public static bool HasCode(this Error error, string code, StringComparison comparisonType)
   {
     return error.Code?.Equals(code, comparisonType) ?? false;
--- src/ModResults/ErrorHelpers.cs
+++ src/ModResults/ErrorHelpers.cs
@@ -1,6 +1,12 @@
 ﻿namespace ModResults;
 internal static class ErrorHelpers
 {
+  /// <summary>
+  /// Creates an <see cref="Error"/> instance from an <see cref="Exception"/>.
+  /// </summary>
+  /// <param name="exception"></param>
+  /// <param name="failureType">Failure type that created error will be associated with.</param>
+  /// <returns></returns>
   public static Error ToError(this Exception exception, FailureType failureType)
   {
     return new Error(
@@ -8,6 +14,12 @@ internal static class ErrorHelpers
         code: Error.GetDefaultCode(failureType));
   }
 
+  /// <summary>
+  /// Creates an <see cref="Error"/> instance from an error message.
+  /// </summary>
+  /// <param name="errorMessage"></param>
+  /// <param name="failureType">Failure type that created error will be associated with.</param>
+  /// <returns></returns>
   public static Error ToError(this string errorMessage, FailureType failureType)
   {
     return new Error(
